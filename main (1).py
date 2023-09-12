da1e358163808ed2988a41ec8c574bb37a92969e
abishayanadarajan <144689753+abishayanadarajan@users.noreply.github.com>
2023-09-12 12:08:06 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+#Default function to implement conditions to check leap Year def CheckLeap(Year):
+#Checking if the given Year is leap Year
+if((Year % 400 == 0)or
+   (Year % 100 != 0)and
+   (Year % 4 == 0)):
+  print("Given Year is a leap Year");
+#Else it is not a leap Year
+else:
+  print("Given Year is not a leap Year")
+  #Taking an input year from user Year=int(input("Enter the number:"))
+  #printing result
+  checkLeap(Year)
